dc6858e4f81b459962c7ff9f9f2fe9888ee6304f
Stefan Härter <sha@rother-oss.com>
2024-06-03 14:01:21 +0200

    Implemented changes to respect url change to otobo.io.

--- Kernel/TidyAll/Plugin/OTOBO/SOPM/RequiredElements.pm
+++ Kernel/TidyAll/Plugin/OTOBO/SOPM/RequiredElements.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -32,7 +32,7 @@ sub transform_source {
     $Code =~ s{ OTOBO [ ]+ GmbH }{Rother OSS GmbH}xmsg;
 
     # Replace <URL>http://otobo.org/</URL> with <URL>https://otobo.org/</URL>
-    $Code =~ s{ ^ ( \s* ) \< URL \> .+? \< \/ URL \> }{$1<URL>https://otobo.de/</URL>}xmsg;
+    $Code =~ s{ ^ ( \s* ) \< URL \> .+? \< \/ URL \> }{$1<URL>https://otobo.io/</URL>}xmsg;
 
     # Replace Version
     $Code =~ s{ <Version> [^<>\n]* <\/Version> }{<Version>0.0.0</Version>}xmsg;
