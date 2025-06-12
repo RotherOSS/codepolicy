13b92a17928c0151fec77147b67cf0237456886a
bernhard <Bernhard.Schmalhofer@gmx.de>
2025-06-12 12:11:01 +0200

    Add comment about suprising effect of the regex
    just because I was surpriced about CodePolicy complaining about DateTime::TimeZone::ICal.

--- Kernel/TidyAll/Plugin/OTOBO/Perl/SyntaxCheck.pm
+++ Kernel/TidyAll/Plugin/OTOBO/Perl/SyntaxCheck.pm
@@ -43,6 +43,9 @@ sub validate_source {
     # Allow important modules that come with the Perl core or are external
     #   dependencies of OTOBO and can thus be assumed as being installed.
     #   Some common modules that are used during development are also not stripped.
+    #
+    # Note that this is not fool proof. Modules like DateTime::TimeZone::ICal would
+    # also not be stripped as the that module name contains the substring 'DateTime'.
     my @AllowedExternalModules = qw(
         vars
         constant
@@ -107,9 +110,9 @@ sub validate_source {
 
     # say STDERR $CleanedSource;
 
-    my $TempFile = File::Temp->new();
+    my $TempFile = File::Temp->new;
     print $TempFile $CleanedSource;
-    $TempFile->flush();
+    $TempFile->flush;
 
     # syntax check
     my $ErrorMessage;
