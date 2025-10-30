e8942a954f8b86d58b7c462749e741c8619e4060
chz <chz@rother-oss.com>
2025-10-30 07:45:55 +0000

    Issue #24: Improve ESLint config
    Integrate recommended plugin configuration
    add global variables
    add ignore paths for third-party and cache files
    ignore CKEditor5Wrapper
    set problematic rules from 'error' to 'warn'

--- Kernel/TidyAll/Plugin/OTOBO/JavaScript/ESLint/eslint.config.mjs
+++ Kernel/TidyAll/Plugin/OTOBO/JavaScript/ESLint/eslint.config.mjs
@@ -6,30 +6,46 @@ import noWindow from "./Rules/no-window.mjs";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { sourceType: "commonjs" } 
-  },
+  pluginJs.configs.recommended,
   {
     languageOptions: { 
+      sourceType: "commonjs", 
       globals: {
         ...globals.browser, 
         ...globals.jquery,
         isJQueryObject: "readonly",
         CKEditor5Wrapper: "readonly",
-        CKEditor5Plugins: "readonly"
+        CKEditor5Plugins: "readonly",
+        CKEditor5CoreTranslations: "readonly",
+        CKEditorInstances: "readonly",
+        QUnit: "readonly",
+        CodeMirror: "readonly"
       }
     },
+    ignores: [
+      "var/httpd/htdocs/js/thirdparty/**",
+      "var/httpd/htdocs/js/js-cache/**",
+      "var/httpd/htdocs/js/Core.UI.CKEditor5Wrapper.js"  // has to be ignored due to using import/export in a non-module file
+    ],
+    files: ["**/*.js"], 
     plugins: {
-      custom: {
+      otobo: {
         rules: {
           'no-window': noWindow
         }
       }
     },
     rules: {
-      'custom/no-window': 'error' // Enable the custom rule with proper namespace
+      'otobo/no-window': 'error',
+      'camelcase': 'warn',
+      // The following rules are temparorily set to 'warn' to avoid breaking existing code
+
+      'no-prototype-builtins': 'warn',
+      'no-unused-vars': 'warn',
+      'no-redeclare': 'warn',
+      'no-undef': 'warn',
+      'no-useless-escape': 'warn'
+
     }
-  },
-  pluginJs.configs.recommended
+  }
 ];
